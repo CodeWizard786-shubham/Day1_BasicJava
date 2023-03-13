999ce2656d3c4d30317ce8c19e7a82b8ebbde4d9
shubham shirke <shirkeshubham84@gmail.com>
2023-03-13 17:47:49 +0530

    Basic Java Programs

---
+++ src/DataTypes.java
@@ -0,0 +1,9 @@
+public class DataTypes {
+    public static void main(String[] args){
+        double a=34.32;
+        System.out.println("Integer:"+(int)a);
+        System.out.println("Float:"+(float)a);
+        System.out.println("Double:"+(double)a);
+        System.out.println("long:"+(long)a);
+    }
+}
